8e50e4be5e09eda200102648030458e31f910060
Tomáš Grusz <t.grusz@make.com>
2024-01-24 10:12:20 +0100

    Add metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,33 @@ import "./globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Terren",
-  description: "Procedural Map Generator & 3D Visualiser",
+  title: "Terren: Map Generator & Visualizer",
+  description: "The next generation of map generators & visualizers.",
+  applicationName: 'terren',
+  authors: [{ name: 'Tomáš Grusz' }],
+  keywords: 'map, generator, procedural, 3d, visualiser, terrain, heightmap, react, threejs, nextjs, typescript',
+  viewport: { width: 'device-width', initialScale: 1 },
+  robots: { index: true, follow: true },
+  alternates: { canonical: 'https://terren.grusz.dev' },
+  openGraph: {
+    type: 'website',
+    url: 'https://terren.grusz.dev',
+    title: 'Terren: Map Generator & 3D Visualizer',
+    description:
+      "The next generation of map generators & visualizers.",
+    siteName: 'Terren',
+    locale: 'en_IE',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    creator: '@gruszdev',
+    title: 'Terren: Map Generator & 3D Visualizer',
+    description:
+      "The next generation of map generators & visualizers.",
+  },
+  other: {
+    'msapplication-TileColor': '#191724',
+  },
 };
 
 export default function RootLayout({
---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Terren: Map Generator & Visualizer',
+        short_name: "Terren",
+        description: "The next generation of map generators & visualizers.",
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#1E283A',
+        theme_color: '#1E283A',
+        icons: [
+            {
+                src: "/icon-192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                src: "/icon-512.png",
+                sizes: "512x512",
+                type: "image/png"
+            },
+        ],
+    }
+}
