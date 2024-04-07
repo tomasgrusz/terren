3a725702d3f7c6b7c89d5f0c53ebaa429840fc9a
Tomáš Grusz <85355882+tomasgrusz@users.noreply.github.com>
2024-04-07 09:01:06 +0200

    fix(lint): indentation

--- app/page.tsx
+++ app/page.tsx
@@ -12,28 +12,28 @@ export default function Home() {
   return (
     <main className="h-lvh">
       <TerrainContextProvider>
-          <ResizablePanelGroup direction="horizontal">
-            <ResizablePanel defaultSize={70}>
-              <ResizablePanelGroup direction="vertical">
-                <ResizablePanel defaultSize={70}>
-                  <Scene />
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel
-                  defaultSize={30}
-                  minSize={5}
-                  maxSize={30}
-                  className="overflow-x-auto"
-                >
-                  <Maps />
-                </ResizablePanel>
-              </ResizablePanelGroup>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel defaultSize={20} minSize={20} maxSize={50}>
-              <Menu />
-            </ResizablePanel>
-          </ResizablePanelGroup>
+        <ResizablePanelGroup direction="horizontal">
+          <ResizablePanel defaultSize={70}>
+            <ResizablePanelGroup direction="vertical">
+              <ResizablePanel defaultSize={70}>
+                <Scene />
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel
+                defaultSize={30}
+                minSize={5}
+                maxSize={30}
+                className="overflow-x-auto"
+              >
+                <Maps />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={20} minSize={20} maxSize={50}>
+            <Menu />
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </TerrainContextProvider>
     </main>
   );
